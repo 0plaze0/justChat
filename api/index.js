1f98b2d6fb98cdba525c8ed9bfabfc44da8ad098
plaze <banikakash131@gamil.com>
2024-01-24 11:50:23 +0530

    feat: add authenticate route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import cors from "cors";
 
+import { router as authenticate } from "./routes/authenticate";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//middleware
 app.use(cors({ origin: true }));
+
+//routes
+app.use("/authenticate", authenticate);
 
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
