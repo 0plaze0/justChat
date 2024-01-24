695f17beaf93df04a24ae0346ca41c127f8f7e8f
plaze <banikakash131@gamil.com>
2024-01-24 13:13:40 +0530

    feat: send data to chatengine

--- api/controllers/authenticateController.js
+++ api/controllers/authenticateController.js
@@ -1,5 +1,21 @@
+import { chatApi } from "../config/chatEngine.js";
+
 const userData = async (req, res) => {
   const { username } = req.body;
+
+  try {
+    const result = await chatApi.put(
+      "/users",
+      {
+        username: username,
+        sercet: username,
+        first_name: username,
+      },
+      { headers: { "Private-Key": "XXXX" } }
+    );
+  } catch (err) {
+    console.log(err.message);
+  }
   return res.json({ username: username, sercet: "sha256..." });
 };
 
